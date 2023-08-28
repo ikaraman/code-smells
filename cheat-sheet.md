1796dc9a06dfe4f087cbc9e18933724a22f5e0ef
Ivan Karaman <4237268+ikaraman@users.noreply.github.com>
2023-08-28 21:03:40 +1200

    Add some tests + started a cheat sheet

---
+++ cheat-sheet.md
@@ -0,0 +1,25 @@
+# Code smells
+List of common issues on the example of the Javascript + Cypress.io
+
+## Test content and structure affecting efficiency
+1. duplicated code
+1. redundant tests (form of duplication)
+1. lack of/bad assertions
+1. brittle selectors (not using unique id/class or selecting by text/appearance)
+1. name mismatch with reality of the:
+   1. test
+   1. variable/function/alias/etc.
+1. test is too long & does too many things
+1. wrong layer of the test pyramid (should've been a unit test)
+1. explicit HTML element waits
+
+## Programming Language issues
+
+
+## Nitpicks
+
+Nitpicks
+1. spacing between
+   1. tests/describes
+   1. ...
+1. lack of consistency
--- cypress/e2e/awesome.test.cy.js
+++ cypress/e2e/awesome.test.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+it('first test', () => {
+  cy.visit('https://magento.softwaretestingboard.com/')
+})
+it('Navigate to sale', () => {
+  cy.visit('https://magento.softwaretestingboard.com/')
+  cy.get('.navigation > ul > li').last().click()
+
+  cy.url().should('contain', 'magento.softwaretestingboard.com')
+})
+
+it.only('Buy something', () => {
+  cy.visit('https://magento.softwaretestingboard.com/')
+  cy.get('.action.more.button').click()
+  cy.get('.product-image-photo').first().click()
+  cy.get('.swatch-option.text').first().click()
+  cy.get('.swatch-option.color').first().click()
+  cy.get('.action.primary.tocart').click()
+  cy.get('div').contains('You added')
+  cy.get('.action.showcart').click()
+  cy.get('div').contains('Proceed to Checkout').click()
+  cy.url().should('contain', 'magento.softwaretestingboard.com/checkout/')
 })
